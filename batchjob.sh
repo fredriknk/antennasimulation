e1467b42979ea566fd19c0ef12b7ef974896a0ca
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2023-09-11 15:47:04 +0200

    fixes

--- batchjob.sh
+++ batchjob.sh
@@ -11,6 +11,6 @@ stopW1=70
 numsW1=10
 meshsize=0.0012
 
-sh antenna.SLURM.sh 700, 733, $numsL3,$startL6, $stopL6 $numsL6 $startW1 $stopW1 $numsW1 $meshsize
-sh antenna.SLURM.sh 734, 766, $numsL3,$startL6, $stopL6 $numsL6 $startW1 $stopW1 $numsW1 $meshsize
-sh antenna.SLURM.sh 767, 800, $numsL3,$startL6, $stopL6 $numsL6 $startW1 $stopW1 $numsW1 $meshsize
+sbatch antenna.SLURM.sh 700, 733, $numsL3,$startL6, $stopL6 $numsL6 $startW1 $stopW1 $numsW1 $meshsize
+sbatch antenna.SLURM.sh 734, 766, $numsL3,$startL6, $stopL6 $numsL6 $startW1 $stopW1 $numsW1 $meshsize
+sbatch antenna.SLURM.sh 767, 800, $numsL3,$startL6, $stopL6 $numsL6 $startW1 $stopW1 $numsW1 $meshsize
