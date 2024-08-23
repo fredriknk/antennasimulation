0de2f2afd08d60373b6db968908b2cbd218d4950
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2024-08-23 14:28:07 +0200

    Batchtesting

--- OPTIMIZEANTENNASCRIPT.m
+++ OPTIMIZEANTENNASCRIPT.m
@@ -1,2 +1,2 @@
-optimizeAntennaMultiVars({"L3","L6","D5"}, {[15e-3, 20.5e-3],[1e-3, 10e-3],[1e-3, 10e-3]}, 'minimum', 's11',50, 1.2e-3)
+optimizeAntennaMultiVars({"L3","L6","D5","W1",'W2', 'W3'}, {[15e-3, 20.5e-3],[1e-3, 10e-3],[1e-3, 10e-3],[0.15e-3, 1e-3], [0.15e-3, 1e-3], [0.15e-3, 1e-3]}, 'minimum', 's11',50, 1.2e-3)
 %optimizeAntennaMultiVars({"W1",'W2', 'W3'}, {[0.15e-3, 1e-3], [0.15e-3, 1e-3], [0.15e-3, 1e-3]}, 'match', 'impedance', 50, 1.2e-3)
--- antenna.SLURM.sh
+++ antenna.SLURM.sh
@@ -3,13 +3,13 @@
 #SBATCH --job-name=Ansim #Name of the job
 #
 ## Wall time limit:
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 #
 ####Partition
 #SBATCH --partition smallmem
 ## Other parameters:
-#SBATCH --cpus-per-task 16 #Number of cpus the job will use
-#SBATCH --mem=64G #Memory RAM
+#SBATCH --cpus-per-task 12 #Number of cpus the job will use
+#SBATCH --mem=16G #Memory RAM
 #SBATCH --nodes 1
 #SBATCH -o logs/slurm-%x_%j.out    #Standar output message
 #SBATCH -e logs/slurm-%x_%j.err    #Standar error message
@@ -21,12 +21,16 @@ echo "I am running on the NODE $SLURM_NODELIST"
 echo "I am running with $SLURM_CPUS_ON_NODE cpus"
 echo "Starting $SLURM_JOB_ID at"
 date
+
+command =$1
+echo $command
+
 echo "Loading modules"
 module purge
 module load MATLAB/2023b
 echo "Module Loaded"
 echo "Starting Matlab"
 timestamp=$(date +"%Y_%m_%d_%H_%M_%S")
-matlab -noFigureWindows -nosplash -nodesktop -r OPTIMIZEANTENNASCRIPT | tee logs/$timestamp_matlab-.out
+matlab -noFigureWindows -nosplash -nodesktop -r $command
 echo "Ending $SLURM_JOB_ID at"
 date
