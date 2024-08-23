d71d18fd71907e4611ba9b415470c30e66d0bb0b
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2024-08-23 14:38:11 +0200

    Batchtesting

--- batchjob.sh
+++ batchjob.sh
@@ -8,13 +8,12 @@ f="optimizeAntennaMultiVars({'L3','L6','D5','W1','W2','W3'}, {[15e-3, 20.5e-3],[
 g="optimizeAntennaMultiVars({'L3','L6','D5','W1','W2','W3'}, {[15e-3, 20.5e-3],[7e-3, 8e-3],[1e-3, 10e-3],[0.15e-3, 1e-3], [0.15e-3, 1e-3], [0.15e-3, 1e-3]}, 'minimum', 's11', 50, 1.2e-3); exit;"
 h="optimizeAntennaMultiVars({'L3','L6','D5','W1','W2','W3'}, {[15e-3, 20.5e-3],[8e-3, 9e-3],[1e-3, 10e-3],[0.15e-3, 1e-3], [0.15e-3, 1e-3], [0.15e-3, 1e-3]}, 'minimum', 's11', 50, 1.2e-3); exit;"
 i="optimizeAntennaMultiVars({'L3','L6','D5','W1','W2','W3'}, {[15e-3, 20.5e-3],[9e-3, 10e-3],[1e-3, 10e-3],[0.15e-3, 1e-3], [0.15e-3, 1e-3], [0.15e-3, 1e-3]}, 'minimum', 's11', 50, 1.2e-3); exit;"
-j="optimizeAntennaMultiVars({'L3','L6','D5','W1','W2','W3'}, {[15e-3, 20.5e-3],[10e-3, 11e-3],[1e-3, 10e-3],[0.15e-3, 1e-3], [0.15e-3, 1e-3], [0.15e-3, 1e-3]}, 'minimum', 's11', 50, 1.2e-3); exit;"
 
-#sbatch antenna.SLURM.sh $a
-#sbatch antenna.SLURM.sh $b
-#sbatch antenna.SLURM.sh $c
-#sbatch antenna.SLURM.sh $d
-#sbatch antenna.SLURM.sh $e
-#sbatch antenna.SLURM.sh $f
-#sbatch antenna.SLURM.sh $g
+sbatch antenna.SLURM.sh $b
+sbatch antenna.SLURM.sh $c
+sbatch antenna.SLURM.sh $d
+sbatch antenna.SLURM.sh $e
+sbatch antenna.SLURM.sh $f
+sbatch antenna.SLURM.sh $g
 sbatch antenna.SLURM.sh	$h
+sbatch antenna.SLURM.sh	$i
